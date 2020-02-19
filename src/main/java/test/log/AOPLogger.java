8ffe60294059bfd50991e8a9081b55d456f4a4cf
AndChelp <AndChelp@yandex.ru>
2020-02-19 23:10:36 +0500

    q

--- src/main/java/test/log/AOPLogger.java
+++ src/main/java/test/log/AOPLogger.java
@@ -1,5 +1,7 @@
 package test.log;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
@@ -10,4 +12,8 @@ import java.util.logging.Logger;
 public class AOPLogger {
     private Logger logger = Logger.getLogger(AOPLogger.class.getName());
 
+    @AfterReturning(value = "execution(* test.controller.UsersController.read())", returning = "result")
+    public void logPoint(JoinPoint joinPoint, Object result) {
+        System.out.println(result.toString());
+    }
 }
